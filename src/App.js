1434f70d5e8b96a64e2cda59be68ec5b4e14cd0c
anhyunji494 <anhyunji494@naver.com>
2024-10-16 22:09:36 +0900

    1016

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LocalFresh from "./pages/LocalFresh/LocalFresh";
 import Products from "./pages/Products/Products";
 import Story from "./pages/Story/Story";
